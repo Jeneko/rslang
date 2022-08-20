a1a6ed1f7c75cec68f06ffa6cca60e25b10bceed
Jeneko <jenekomail@gmail.com>
2022-08-20 13:32:17 +0300

    feat: add "createUser" to API

--- src/API/index.ts
+++ src/API/index.ts
@@ -17,3 +17,19 @@ export async function getWord(id: number): Promise<Word> {
 
   return result;
 }
+
+export async function createUser(name: string, email: string, password: string): Promise<boolean> {
+  const url = `${SOURCE}/${Endpoints.users}`;
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name,
+      email,
+      password,
+    }),
+  };
+  const response = await fetch(url, options);
+
+  return response.ok;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -18,6 +18,7 @@ export enum PageName {
 
 export enum Endpoints {
   words = 'words',
+  users = 'users',
   // TODO: add other endpoints
 }
 
