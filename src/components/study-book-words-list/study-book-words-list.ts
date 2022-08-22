da24b6d1cd79e7d31874f3bee3f80146bff7e2a2
Jeneko <jenekomail@gmail.com>
2022-08-22 15:15:20 +0300

    refactor: improve readability - replace if statement with ternary operator

--- src/components/study-book-words-list/study-book-words-list.ts
+++ src/components/study-book-words-list/study-book-words-list.ts
@@ -10,21 +10,18 @@ export default async function getStudyBookWordsList(): Promise<HTMLElement> {
   const elem = document.createElement('div');
   elem.className = 'study-book-words-list';
 
-  let innerHTML = `
+  const standardBookChapterHTML = `
     <h2 class="study-book-chapter-heading">Chapter ${Number(curState.studyBookChapter) + 1} | Page ${Number(curState.studyBookPage) + 1}</h2>
     <ol></ol>
   `;
 
-  // TODO: write an actual link to the login page
-  if (Number(curState.studyBookChapter) >= 6) {
-    innerHTML = `
-      <h2 class="study-book-chapter-heading">Chapter ${Number(curState.studyBookChapter) + 1} | User Words</h2>
-      <p>Chapter 7 contains the most difficult words user selected manually. Please, <a href="#">Login or Register</a> to start using this chapter.</p>
-      <ol></ol>
-    `;
-  }
+  const userBookChapterHTML = `
+    <h2 class="study-book-chapter-heading">Chapter ${Number(curState.studyBookChapter) + 1} | User Words</h2>
+    <p>Chapter 7 contains the most difficult words user selected manually. Please, <a href="#">Login or Register</a> to start using this chapter.</p>
+    <ol></ol>
+  `;
 
-  elem.innerHTML = innerHTML;
+  elem.innerHTML = (Number(curState.studyBookChapter) >= 6) ? userBookChapterHTML : standardBookChapterHTML;
 
   const words = await getWords(curState.studyBookChapter, curState.studyBookPage);
   const orderedList = elem.querySelector('ol') as HTMLElement;
