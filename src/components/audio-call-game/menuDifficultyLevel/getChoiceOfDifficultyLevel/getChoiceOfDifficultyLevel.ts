2cac8e8d02a5dae29267cbf9e39ac4d726979cba
Artem Dubovov <artem.dubovov@bk.ru>
2022-08-23 22:10:17 +0300

    refactor: remove commented code

--- src/components/audio-call-game/menuDifficultyLevel/getChoiceOfDifficultyLevel/getChoiceOfDifficultyLevel.ts
+++
@@ -1,7 +0,0 @@
-// import startNewGame from './startNewGame/game';
-
-// export default function getChoiceOfDifficultyLevel(elem: HTMLElement) {
-//   const menuLevels = elem.querySelector('.btn-group');
-//   const game = menuLevels?.addEventListener('click', startNewGame);
-//   return game;
-// }
