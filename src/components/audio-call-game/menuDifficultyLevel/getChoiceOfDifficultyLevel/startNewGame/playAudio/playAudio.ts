49ebfd39ed546146abab736af0361d0d0c0af8ed
Artem Dubovov <artem.dubovov@bk.ru>
2022-08-24 01:15:08 +0300

    refactor: refactor function playAudio

--- src/components/audio-call-game/menuDifficultyLevel/getChoiceOfDifficultyLevel/startNewGame/playAudio/playAudio.ts
+++ src/components/audio-call-game/menuDifficultyLevel/getChoiceOfDifficultyLevel/startNewGame/playAudio/playAudio.ts
@@ -2,12 +2,6 @@ import { SOURCE } from 'API/index';
 
 export default async function playAudio(fileName: string): Promise<void> {
   const url = `${SOURCE}/${fileName}`;
-  const audio = new AudioContext();
-  const response = await fetch(url);
-  const arrayBuffer = await response.arrayBuffer();
-  const decodedAudio = await audio.decodeAudioData(arrayBuffer);
-  const audioModule = audio.createBufferSource();
-  audioModule.buffer = decodedAudio;
-  audioModule.connect(audio.destination);
-  audioModule.start(audio.currentTime);
+  const audio = new Audio(url);
+  audio.play();
 }
