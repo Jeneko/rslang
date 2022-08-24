17bd915a6c23d9b1780fb7a947565fc8f7e451b1
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-08-24 15:13:16 +0300

    feat: add get page functions

--- src/components/about-team/about-team.ts
+++ src/components/about-team/about-team.ts
@@ -1,10 +1,13 @@
+import './about-team.css';
+
 export default function getAboutTeam(): HTMLElement {
   const elem = document.createElement('div');
   elem.className = 'about-team';
 
   elem.innerHTML = `
-    <br><br>
-    <button class="btn btn-primary">Let's start</button>
+    <div class="container">
+      <h1>Our Team</h1>
+    </div>
   `;
 
   return elem;
