f1fb6852be9943c669b0cc95f1f3971d990648cc
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-08-24 13:53:27 +0300

    feat: finish footer

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -5,14 +5,14 @@ export default function getFooter(): HTMLElement {
   elem.className = 'footer';
 
   elem.innerHTML = `
-    <div class='container>
+    <div class="container">
       <div class="footer-container">
-        <ul class="footer__team">
-          <li><a href="https://github.com/Jeneko" class="git" target="_blank">Jeneko</a></li>
-          <li><a href="https://github.com/ArtemDubovov" class="git" target="_blank">ArtemDubovov</a></li>
-          <li><a href="https://github.com/dmitrykaliasinski" class="git" target="_blank">dmitrykaliasinski</a></li>
-        </ul>
-        <p>2022</p>
+        <div class="footer__team">
+          <a href="https://github.com/Jeneko" class="link-light" target="_blank">Jeneko</a>
+          <a href="https://github.com/ArtemDubovov" class="link-light" target="_blank">ArtemDubovov</a>
+          <a href="https://github.com/dmitrykaliasinski" class="link-light" target="_blank">dmitrykaliasinski</a>
+        </div>
+        <div class="footer-year">2022</div>
         <a href="https://rs.school/js/" class="footer-rsschool" target="_blank"></a>
       </div>
     </div>
