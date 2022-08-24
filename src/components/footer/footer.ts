718b9e5637c60d5f7d5cfe174fd543907c6cd832
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-08-24 12:58:41 +0300

    feat: create Html structure

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -8,8 +8,8 @@ export default function getFooter(): HTMLElement {
     <div class='container>
       <div class="footer-container">
         <ul class="footer__team">
-          <li><a href="https://github.com/dmitrykaliasinski" class="git" target="_blank">dmitrykaliasinski</a></li>
-          <li><a href="https://github.com/dmitrykaliasinski" class="git" target="_blank">dmitrykaliasinski</a></li>
+          <li><a href="https://github.com/Jeneko" class="git" target="_blank">Jeneko</a></li>
+          <li><a href="https://github.com/ArtemDubovov" class="git" target="_blank">ArtemDubovov</a></li>
           <li><a href="https://github.com/dmitrykaliasinski" class="git" target="_blank">dmitrykaliasinski</a></li>
         </ul>
         <p>2022</p>
