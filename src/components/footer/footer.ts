cfa6524b4d6856f2a3a0dfb237fd053b67c4d774
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-08-24 12:48:44 +0300

    init: start work on footer

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -5,7 +5,17 @@ export default function getFooter(): HTMLElement {
   elem.className = 'footer';
 
   elem.innerHTML = `
-    <h3>Footer</h3>
+    <div class='container>
+      <div class="footer-container">
+        <ul class="footer__team">
+          <li><a href="https://github.com/dmitrykaliasinski" class="git" target="_blank">dmitrykaliasinski</a></li>
+          <li><a href="https://github.com/dmitrykaliasinski" class="git" target="_blank">dmitrykaliasinski</a></li>
+          <li><a href="https://github.com/dmitrykaliasinski" class="git" target="_blank">dmitrykaliasinski</a></li>
+        </ul>
+        <p>2022</p>
+        <a href="https://rs.school/js/" class="footer-rsschool" target="_blank"></a>
+      </div>
+    </div>
   `;
 
   return elem;
