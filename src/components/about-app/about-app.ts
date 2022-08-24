305a51ab01b55204e6cab30aee84cd3093fa9a85
Artem Dubovov <artem.dubovov@bk.ru>
2022-08-24 17:52:12 +0300

    feat: add content and image for about-app page

--- src/components/about-app/about-app.ts
+++ src/components/about-app/about-app.ts
@@ -1,5 +1,6 @@
-import rsschoolLogoUrl from './rsschool-logo.svg';
+// import rsschoolLogoUrl from './rsschool-logo.svg';
 import './about-app.css';
+import svg from './learn.svg';
 
 // Dummy function content (to be replaced)
 export default function getAboutApp(): HTMLElement {
@@ -7,10 +8,22 @@ export default function getAboutApp(): HTMLElement {
   elem.className = 'about-app';
 
   elem.innerHTML = `
-    <img src="${rsschoolLogoUrl}">
-    <br><br>
-    <button class="btn btn-primary">Let's start</button>
+    <div class="container text-center">
+      <div class="row justify-content-around about-wrapper">
+        <div class="col-sm-10 col-md-5 align-self-center about-wrapper--block">
+          <h1 class="about-app--title display-1">RS Lang</h1>
+          <h2 class="about-app--subtitle display-4">Learning English is very easy and fun!</h2>
+          <p class="about-app--content">
+            Memorize words using the dictionary and playing mini games,
+            listen to pronunciation, study transcription, examples in sentences,
+            analyze your achievements through detailed statistics and all this for free on RS Lang!
+          </p>
+        </div>
+        <div class="col-sm-10 col-md-5 align-self-center about-wrapper--block">
+          <img src="${svg}" class="about-wrapper--img" alt="background-image">
+        </div>
+      </div>
+    </div>
   `;
-
   return elem;
 }
