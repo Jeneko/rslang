fcfe515ae81801b0bdc98aec8baed097945285a3
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-08-25 12:07:28 +0300

    refactor: currect html on bootstrap coloms

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -5,15 +5,17 @@ export default function getFooter(): HTMLElement {
   elem.className = 'footer';
 
   elem.innerHTML = `
-    <div class="container">
-      <div class="footer-container">
-        <div class="footer__team">
+    <div class="container text-center">
+      <div class="row align-items-center">
+        <div class="col">
           <a href="https://github.com/Jeneko" class="link-light" target="_blank">Jeneko</a>
           <a href="https://github.com/ArtemDubovov" class="link-light" target="_blank">ArtemDubovov</a>
           <a href="https://github.com/dmitrykaliasinski" class="link-light" target="_blank">dmitrykaliasinski</a>
         </div>
-        <div class="footer-year">2022</div>
-        <a href="https://rs.school/js/" class="footer-rsschool" target="_blank"></a>
+        <div class="col">2022</div>
+        <div class="col">
+          <a href="https://rs.school/js/" class="footer-rsschool" target="_blank"></a>
+        </div>
       </div>
     </div>
   `;
