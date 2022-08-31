d3545ba66468d3f160a0bdb638384c1340f336c7
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-08-31 22:26:09 +0300

    refactor: delete to many indentations in renderModal

--- src/components/sprint-game/modal-lvl.ts
+++ src/components/sprint-game/modal-lvl.ts
@@ -2,20 +2,20 @@ import { getWords } from 'API/index';
 import { Word } from 'types/index';
 
 export const renderModal = `
-    <h1 class="page-heading">
-      <span class="page-heading__rslang">RSLang</span> Sprint
-    </h1>
-    <div class="sprint">
-      <div class="modal-lvl">
-        <p>From Easy to most Hard</p>
-        <button class="btn btn-lvl btn-primary" data-lvl="0">1</button>
-        <button class="btn btn-lvl btn-primary" data-lvl="1">2</button>
-        <button class="btn btn-lvl btn-primary" data-lvl="2">3</button>
-        <button class="btn btn-lvl btn-primary" data-lvl="3">4</button>
-        <button class="btn btn-lvl btn-primary" data-lvl="4">5</button>
-        <button class="btn btn-lvl btn-primary" data-lvl="5">6</button>
-      </div>
+  <h1 class="page-heading">
+    <span class="page-heading__rslang">RSLang</span> Sprint
+  </h1>
+  <div class="sprint">
+    <div class="modal-lvl">
+      <p>From Easy to most Hard</p>
+      <button class="btn btn-lvl btn-primary" data-lvl="0">1</button>
+      <button class="btn btn-lvl btn-primary" data-lvl="1">2</button>
+      <button class="btn btn-lvl btn-primary" data-lvl="2">3</button>
+      <button class="btn btn-lvl btn-primary" data-lvl="3">4</button>
+      <button class="btn btn-lvl btn-primary" data-lvl="4">5</button>
+      <button class="btn btn-lvl btn-primary" data-lvl="5">6</button>
     </div>
+  </div>
 `;
 
 export const generateWords = async (lvl: number, currentPage: number): Promise<Word[]> => {
