668ae03d33cb33f6d3f9de418761afdc1d180cd6
Artem Dubovov <artem.dubovov@bk.ru>
2022-09-04 04:03:05 +0300

    fix: fix add learn words for statistics

--- src/components/audio-call-game/game/sendingToServer/sendingToServer.ts
+++ src/components/audio-call-game/game/sendingToServer/sendingToServer.ts
@@ -56,6 +56,7 @@ function updateWordStatistic(correctAnswersList: WordWithUserWord[], wrongAnswer
 async function updateGameStatistic(gameStatistics: GameStatistic, wordStatistics: WordsStatistic, correctAnswersList: WordWithUserWord[], wrongAnswersList: WordWithUserWord[], gameState: GameState, userStatistics: Statistic) {
   const lengthDateGame = userStatistics.optional.audiocall.stat.length;
   const lengthDateWords = userStatistics.optional.words.stat.length;
+  userStatistics.learnedWords = wordStatistics.learnedWordsQty;
   userStatistics.optional.audiocall.stat[lengthDateGame - 1].longestRow = gameStatistics.longestRow < gameState.longestStreakForGame ? gameState.longestStreakForGame : gameStatistics.longestRow;
   userStatistics.optional.audiocall.stat[lengthDateGame - 1].rightAnswers += correctAnswersList.length;
   userStatistics.optional.audiocall.stat[lengthDateGame - 1].wrongAnswers += wrongAnswersList.length;
